90df0a577ab2c412ea7ef7081e7963b5f7e45306
Sergio <sergio_jair4@hotmail.com>
2026-04-04 18:29:38 -0600

    feat: add initial Vue components for app layout and pages

---
+++ clothing-web/app/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <header>
+        <div class="logo">MiSitioWeb</div>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <p>Welcome to my app!</p>
+
+    
+    <footer>
+      <p>&copy; 2026 My App. All rights reserved.</p>
+    </footer>
+    
+    <slot />
+  </div>
+</template>
